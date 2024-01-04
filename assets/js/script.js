c5e01ad930416061b1aca9e5bf142020f939937e
Fellipe Santos <fellipeuker12@gmail.com>
2024-01-03 21:11:07 -0300

    script pronto

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,38 @@ Login.addEventListener('click', () => {
 signUp.addEventListener('click', () => {
     LoginContainer.classList.remove('active');
 });
+
+function getSignUpvalues () {
+    const signupName = document.getElementById('Name');
+    const signupMail = document.getElementById('Email');
+    const signupPassword = document.getElementById('Senha')
+    const signUpForm = document.getElementById('Form');
+
+    signUpForm.addEventListener('submit', e => {
+        e.preventDefault();
+        console.log({
+            name: signupName.value,
+            mail: signupMail.value,
+            password: signupPassword.value,
+        });
+
+    })
+}
+
+getSignUpvalues();
+
+function getLoginValues() {
+    const LoginEmail = document.getElementById('EmailLogin');
+    const LoginPassword = document.getElementById('PasswordLogin');
+    const LoginForm = document.getElementById('FormLogin');
+
+    LoginForm.addEventListener('submit', e => {
+        e.preventDefault();
+        console.log({
+            loginemail: LoginEmail.value,
+            loginpassword: LoginPassword.value,
+        });
+    })
+}
+
+getLoginValues();
